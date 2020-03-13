0b728dd5f9ac3e356f1f8c58d4538765378b5ea5
Ugrah <maxmind.ulrich@gmail.com>
2020-03-13 09:36:32 +0100

    Update api Add Verification entity Remove EmailVerification Remove PhoneVerification

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-var_dump( apache_request_headers() ); exit();
+// var_dump( apache_request_headers() ); exit();
 
 // var_dump( apache_request_headers()['authtoken'] ); exit();
 
